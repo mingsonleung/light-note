980e3bb385793462618ff197ee69e95ff2ec0ba4
mingsonleung <83376393+mingsonleung@users.noreply.github.com>
2024-02-22 18:44:20 -0800

    Completed Settings page and added dynamic color schemes

---
+++ app/components/Submitbuttons.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Loader2 } from "lucide-react";
+import { useFormStatus } from "react-dom";
+
+export function SubmitButton() {
+  const { pending } = useFormStatus();
+  return (
+    <>
+      {pending ? (
+        <Button disabled className="w-fit">
+          <Loader2 className="mr-2 w-4 h-4 animate-spin" />
+          Saving
+        </Button>
+      ) : (
+        <Button className="w-fit" type="submit">
+          Save
+        </Button>
+      )}
+    </>
+  );
+}
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,9 @@
 export default function Page() {
   return (
     <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard!</p>
+        <div>
+            <h1>Hello - Dashboard Page</h1>
+        </div>
     </div>
   );
 }
